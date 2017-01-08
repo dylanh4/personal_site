d056602732e11e8b8911a777d3e996d499bb8620
Dylan Holmes <dylanbholmes@gmail.com>
2017-01-08 17:44:25 +1100

    add jquery

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-'use strict';
-
-var menu = document.getElementsByClassName('handle');
-var nav = document.getElementById('header_ul');
-
-function handleClick() {
-  if (nav.className === 'inactive') {
-    nav.className === 'active';
-  }
-  else (nav.className === 'inactive');
-}
-
-menu.addEventListener('click', handleClick);
+$(document).ready(function(){
+  $('#handle').click(function(){
+    $('#header_bar').toggleClass('visible');
+  })
+});
